f47a77562bd97bd2462586fe845758024db95638
sancar bal <sncr.bl@hotmail.com>
2023-03-04 00:28:56 +0300

    Adding some fun to the website

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 `use strict`;
-const randomInt = function (max = 0){
+const randomInt = function (max = 0) {
     return Math.floor(Math.random() * max);
 }
-const backgroundChanger = function(){
-    const element =document.querySelector('.container--div');
+const backgroundChanger = function () {
+    const element = document.querySelector('.container--div');
     element.style.background = `rgb(${randomInt(256)},
     ${randomInt(256)},
     ${randomInt(256)})`
@@ -11,10 +11,36 @@ const backgroundChanger = function(){
 const btnStart = document.querySelector('.btn-start');
 const btnStop = document.querySelector('.btn-stop');
 
-let interval;
-btnStart.addEventListener('click',function (evt){
-    interval = setInterval(backgroundChanger,1000);
+let intervalBackground;
+let intervalPosition;
+let isFun = false;
+let randomSec;
+btnStart.addEventListener('click', function () {
+    if (!isFun){
+        intervalBackground = setInterval(backgroundChanger, 1000)
+        intervalPosition = setInterval(changePosition,300);
+        btnStop.style.visibility = 'visible'
+        randomSec = randomInt(10);
+        setTimeout(() => btnStop.style.visibility = 'hidden',(randomSec - 0.1) * 1000)
+        setTimeout(afterFun,randomSec*1000);
+        isFun = true;
+    }
 })
-btnStop.addEventListener('click',function (ev) {
-    interval && clearInterval(interval);
-})
+
+function clearEnvironment(){
+    clearInterval(intervalBackground);
+    clearInterval(intervalPosition);
+}
+function changePosition() {
+    const i = Math.floor(Math.random() * window.innerWidth) - Number(getComputedStyle(btnStart).width.split('px')[0]);
+    const j = Math.floor(Math.random() * window.innerHeight) - Number(getComputedStyle(btnStart).height.split('px')[0]);
+    btnStop.style.left = i + "px";
+    btnStop.style.top = j + "px";
+}
+function afterFun(){
+    alert('i think it is enough 😊')
+    clearEnvironment();
+    document.querySelector('.container--div').style.background = 'lightgoldenrodyellow';
+    isFun = false;
+    btnStop.removeEventListener('mouseover',changePosition);
+}
